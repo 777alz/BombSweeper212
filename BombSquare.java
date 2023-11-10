9bf4864f319ee5b5ce3a0f80b7fcf9f7ae9138bd
777alz <88681809+777alz@users.noreply.github.com>
2023-11-10 14:53:51 +0000

    - changed around the clicked method so it has a more logical structure - added javadoc

--- BombSquare.java
+++ BombSquare.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class BombSquare extends GameSquare {
     private boolean thisSquareHasBomb = false;
     public static final int MINE_PROBABILITY = 10;
+    //boolean for if a square has already been revealed
     private boolean revealed = false;
 
     public BombSquare(int x, int y, GameBoard board) {
@@ -12,19 +13,30 @@ public class BombSquare extends GameSquare {
         thisSquareHasBomb = (r.nextInt(MINE_PROBABILITY) == 0);
     }
 
+    /**
+     * method for when a square is clicked:
+     * if the square has a bomb, it will display that.
+     * else, if there are no bombs surrounding the square it will reveal the adjacent empty squares.
+     * else, it displays the number of adjacent bombs.
+     *
+     */
     public void clicked() {
+        int bombCount = adjacentBombs();
         if (thisSquareHasBomb) {
             this.setImage("images/bomb.png");
-        } else {
-            int bombCount = adjacentBombs();
+        } else if (bombCount == 0 && !revealed) {
+            revealed = true;
+            revealAdjacentSquares();
+            this.setImage("images/0.png");
+        }
+        else {
             this.setImage("images/" + bombCount + ".png");
-            if (bombCount == 0 && !revealed) {
-                revealed = true;
-                revealSurroundingEmptySquares();
-            }
         }
     }
 
+    /**
+     * @return the number of bombs in the squares adjacent to the current square.
+     */
     private int adjacentBombs() {
         int bombCount = 0;
 
@@ -44,10 +56,13 @@ public class BombSquare extends GameSquare {
         return bombCount;
     }
 
-    private void revealSurroundingEmptySquares() {
+    /**
+     * Reveals the adjacent empty squares.
+     */
+    private void revealAdjacentSquares() {
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
-                if (dx == 0 && dy == 0) continue; // Skip the current square
+                if (dx == 0 && dy == 0) continue; //skip current square
                 int nx = xLocation + dx;
                 int ny = yLocation + dy;
                 if (nx >= 0 && nx < board.getWidth() && ny >= 0 && ny < board.getHeight()) {
